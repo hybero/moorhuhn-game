fa2607404573f1f18f25a535300cb2c246d1d269
hybero <pavol.havlik@gmail.com>
2020-05-29 20:51:13 +0200

    Checkbox class

---
+++ js/Checkbox.js
@@ -0,0 +1,26 @@
+// Checkbox
+const Checkbox = function (id, checked) {
+    this.id = 'checkbox_'+id;
+    this.x = null, this.y = null;
+    this.img = null;
+    this.checked = checked || false;
+    this.imgSrc = null;
+    this.cImgSrc = './assets/graphics/checked.png';
+    this.uImgSrc = './assets/graphics/unchecked.png';
+    if(this.checked == true) {
+        this.imgSrc = this.cImgSrc;
+    } else {
+        this.imgSrc = this.uImgSrc;
+    }
+    this.width = 60; this.height = 60;
+    this.spriteWidth = 200; this.spriteHeight = 200;
+};
+Checkbox.prototype = {
+
+    setPositions: function(x, y) {
+
+        this.x = x;
+        this.y = y;
+    }
+
+};
